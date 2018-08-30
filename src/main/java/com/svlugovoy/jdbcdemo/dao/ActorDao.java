fd21f0c0e736a28dc6cc3f24e62f9d139e40d1d3
svlugovoy <svlugovoy@gmail.com>
2018-08-30 23:14:57 +0300

    Added domain model

---
+++ src/main/java/com/svlugovoy/jdbcdemo/dao/ActorDao.java
@@ -0,0 +1,4 @@
+package com.svlugovoy.jdbcdemo.dao;
+
+public interface ActorDao {
+}
---
+++ src/main/java/com/svlugovoy/jdbcdemo/domain/Actor.java
@@ -0,0 +1,25 @@
+package com.svlugovoy.jdbcdemo.domain;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
+@Builder
+public class Actor {
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private LocalDate birthday;
+    private Character gender;
+    private String instagram;
+
+    private List<Movie> movies;
+}
+
